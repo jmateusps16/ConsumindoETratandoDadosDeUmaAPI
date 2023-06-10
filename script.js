09197727ad8bccb02ff19b615220c3e92d8dcc62
Joao Ferreira <joao.ferreira@equals.com.br>
2023-06-10 14:02:52 -0300

    adicionando mensagem de erro e validação de cep

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 async function buscarEndereco(cep) {
-    if (cep != null && cep.trim() !== '' && !isNaN(cep)) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    if (cep != null && cep.trim() !== '' && !isNaN(cep) && cep.trim().length === 8) {
         try {
             const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
             const data = await response.json();
-        
-            if(data.erro) {
-                throw new Error('Cep não existe!');
+
+            if (data.erro) {
+                const mensagem = "Cep não existe!";
+                mensagemErro.innerHTML = mensagem;
+                throw new Error(mensagem);
             } else {
+                var cidade = document.getElementById('cidade');
+                var logradouro = document.getElementById('endereco');
+                var bairro = document.getElementById('bairro');
+                var estado = document.getElementById('estado');
+
+                cidade.value = data.localidade;
+                logradouro.value = data.logradouro;
+                bairro.value = data.bairro;
+                estado.value = data.uf;
                 return data;
             }
         } catch (error) {
@@ -14,8 +27,14 @@ async function buscarEndereco(cep) {
             throw error;
         }
     } else {
-        return console.error("Nenhum valor informado no cep ou valor não é numérico")
-    }    
+        if (cep.trim().length !== 8) {
+            mensagemErro.innerHTML = "Cep deve conter 8 números";
+        } else {
+            const mensagem = "Nenhum valor informado no cep ou valor não é numérico";
+            mensagemErro.innerHTML = mensagem;
+            return console.error(mensagem)
+        }
+    }
 }
 
 var cep = document.getElementById('cep');
